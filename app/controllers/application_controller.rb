78fe2fc3bbd5b443ae78ca0284fee18f7bb27c8c
Trygve <trygvejohannessen88@gmail.com>
2022-03-12 01:16:51 +0100

    user name phone number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from ActionController::Redirecting::UnsafeRedirectError do
     redirect_to root_url
+  end
+
+  protected
+
+  public def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
   end
 end
