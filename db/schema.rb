7b3197e57339146d9ce5b508cd1a584c9ad66268
Trygve <trygvejohannessen88@gmail.com>
2022-03-12 04:03:57 +0100

    skill

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_11_222358) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_030207) do
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
