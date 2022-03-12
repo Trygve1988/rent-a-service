20efd2d92213cc7e67dff2ccf6641fed62fadc62
Trygve <trygvejohannessen88@gmail.com>
2022-03-12 18:55:11 +0100

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :searches
   resources :user_skills
   resources :sheep
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_135443) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_173150) do
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "searches", force: :cascade do |t|
     t.string "search_word"
     t.datetime "created_at", null: false
@@ -56,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_135443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "user_skills", "skills"
   add_foreign_key "user_skills", "users"
 end
