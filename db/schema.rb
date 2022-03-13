063e240c93ffbc9aee78dd441c059da98ac3fb2c
Trygve <trygvejohannessen88@gmail.com>
2022-03-13 13:47:08 +0100

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_03_12_135443) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "searches", force: :cascade do |t|
     t.string "search_word"
     t.datetime "created_at", null: false
@@ -32,8 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_135443) do
   end
 
   create_table "user_skills", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "skill_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "skill_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["skill_id"], name: "index_user_skills_on_skill_id"
