754aca4dd2dfcd3f0d126f0c3316197a7cdf640c
Trygve <trygvejohannessen88@gmail.com>
2022-03-15 20:57:28 +0100

    autocomplete, dependant destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,8 @@
 class User < ApplicationRecord
-  has_many :user_skills
+  has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  def hasSkill(skill_id)
-    puts "SKiiiiiiiiiil #{skill_id}"
-    hasSkill = false
-    self.user_skills.each do |user_skill|
-      if user_skill.skill_id == skill_id.to_i
-        hasSkill = true
-      end
-    end
-    hasSkill
-  end
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
